e5f0184bdf2cff792bc863779760ea16a6e4e8c0
TobyThorneA <igraman_XX@mail.ru>
2022-09-26 15:21:10 +0300

    сделал моки под объявление

--- js/main.js
+++ js/main.js
@@ -1,20 +1,48 @@
-function getRandomArbitrary(min, max) {
-  if(min >= max || !max){
-    return ' минимальное число должно быть меньше максимального'
-  } else if (min < 0) {
-    return 'минимальное значение должно быть положительным'
+import {getRandomArbitrary,getRandomFraction} from "./util.js";
+
+let advertisements = [];
+const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const checkinTime = ['12:00', '13:00', '14:00'];
+const checkoutTime = ['12:00', '13:00', '14:00'];
+const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator',' conditioner'];
+const photos = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const advertisementsGenerate = () => {
+  for(let i =  0; i < 10; i++) {
+
+    const locationLat = getRandomFraction(35.65000, 37.70000, 5);
+    const locationLng = getRandomFraction(139.70000, 139.8000, 5);
+
+    const advertisement = {
+      author: {
+        avatar: `img/avatars/user${getRandomArbitrary(1,10).toString().padStart(2, 0)}.png`
+      },
+      offer: {
+        title: 'noName',
+        adress: `${locationLat}, ${locationLng}`,
+        price: getRandomArbitrary(1000, 10000),
+        type: types[getRandomFraction(0, 4)],
+        rooms: getRandomArbitrary(1, 10),
+        guests: getRandomArbitrary(1,20),
+        checkIn: checkinTime[0,2],
+        checkOut: checkoutTime[0,2],
+        features: features.slice(0, getRandomArbitrary(0, features.length)),
+        description: 'тут будет описание жилья',
+        photos: photos.slice(0, photos.length)
+      },
+      location: {
+        lat: locationLat,
+        lng: locationLng,
+      },
+    }
+    advertisements.push(advertisement)
   }
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getRandomFraction (min, max, after){
-  if (max <= min || min < 0 || !max || after < 0){
-    // throw Error('Неверно указан диапазон чисел!');
-    return'Неверно указан диапазон чисел!'
-  }else{
-    const random = Math.random() * (max - min) + min;
-    return random.toFixed(after);
-  }
-}
+advertisementsGenerate()
+
+console.log(advertisements)
---
+++ js/util.js
@@ -0,0 +1,22 @@
+export function getRandomArbitrary(min, max) {
+  if(min >= max || !max){
+    return ' минимальное число должно быть меньше максимального'
+  } else if (min < 0) {
+    return 'минимальное значение должно быть положительным'
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function getRandomFraction (min, max, after){
+  if (max <= min || min < 0 || !max || after < 0){
+    // throw Error('Неверно указан диапазон чисел!');
+    return'Неверно указан диапазон чисел!'
+  }else{
+    const random = Math.random() * (max - min) + min;
+    return random.toFixed(after);
+  }
+}
+
+
